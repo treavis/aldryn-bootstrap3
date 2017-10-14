ac226c5415356edd64354490224c8b5e93209bd2
Russell Moffitt <russ@russmoffitt.com>
2017-10-13 17:14:33 -0700

    Django 1.11 compatibility, remove RadioSelectRenderer

--- aldryn_bootstrap3/widgets.py
+++ aldryn_bootstrap3/widgets.py
@@ -2,37 +2,18 @@
 from __future__ import unicode_literals, absolute_import
 
 import django.forms.widgets
+from django.template import loader
 
 from . import constants
 from .conf import settings
 
-
-class ContextRenderer(django.forms.widgets.RadioFieldRenderer):
-    def render(self):
-        from django.template.loader import render_to_string
-        rendered = render_to_string(
-            'admin/aldryn_bootstrap3/widgets/context.html',
-            {'selects': self},
-        )
-        return rendered
-
-
+    
 class Context(django.forms.widgets.RadioSelect):
-    renderer = ContextRenderer
-
-
-class SizeRenderer(django.forms.widgets.RadioFieldRenderer):
-    def render(self):
-        from django.template.loader import render_to_string
-        rendered = render_to_string(
-            'admin/aldryn_bootstrap3/widgets/size.html',
-            {'selects': self},
-        )
-        return rendered
+    template_name = 'admin/aldryn_bootstrap3/widgets/context.html'
 
 
 class Size(django.forms.widgets.RadioSelect):
-    renderer = SizeRenderer
+    template_name = 'admin/aldryn_bootstrap3/widgets/size.html'
 
 
 class Icon(django.forms.widgets.TextInput):
@@ -70,18 +51,8 @@ class MiniTextarea(django.forms.widgets.Textarea):
         super(MiniTextarea, self).__init__(attrs)
 
 
-class LinkOrButtonRenderer(django.forms.widgets.RadioFieldRenderer):
-    def render(self):
-        from django.template.loader import render_to_string
-        rendered = render_to_string(
-            'admin/aldryn_bootstrap3/widgets/link_or_button.html',
-            {'selects': self},
-        )
-        return rendered
-
-
 class LinkOrButton(django.forms.widgets.RadioSelect):
-    renderer = LinkOrButtonRenderer
+    template_name = 'admin/aldryn_bootstrap3/widgets/link_or_button.html'
 
 
 class Responsive(django.forms.widgets.Textarea):
